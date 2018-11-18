5d78221fb3e0e76252c210da71d25857aa0cc20d
Victor de Morais <victordemorais@hotmail.com.br>
2018-11-18 16:21:25 -0200

    teste

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-const a = 12
-var b;
-console.log(a)
-let trab = "asoijaiossaoasatestessss";
+const numberOne = 12;
+const numberTwo = 2;
+
+const trab = 'asoijaiossaoasa';
+
+function sum(a, b) {
+  const text = trab;
+
+  return a + b + text.length();
+}
+
+sum(numberOne, numberTwo);
